4e111f8525bdcbd811f69daf03ec5f62016dcbed
Albin Gunnarsson <ziboms@gmail.com>
2025-06-04 12:41:50 +0200

    SVGP server and gp saving ( NOT TESTED )

--- residual_modelling/inference/fastserver.py
+++ residual_modelling/inference/fastserver.py
@@ -30,7 +30,7 @@ gp_wy = None
 gp_wz = None
 
 likelihood   = None
-gp_scalar    = None
+gp_scalwer    = None
 _model_choice= "knn"
 _dof_choice  = "6"
 
@@ -38,29 +38,28 @@ weight = np.array([1]*6 +[1]*6 + [1]*6, dtype=np.float32)
 
 # -------------------------------------
 def load_all(model_choice, dof_choice):
-    global knn_model, gp_x, gp_y, gp_z, gp_wx, gp_wy, gp_wz, likelihood, gp_scalar, _model_choice, _dof_choice, weight
+    global knn_model, gp_x, gp_y, gp_z, gp_wx, gp_wy, gp_wz, likelihood, gp_scaler, _model_choice, _dof_choice, weight
 
     _model_choice = model_choice
     _dof_choice = dof_choice
 
     if dof_choice == "6":
         knn_path = "knn_6dof.pkl"
-
-        gp_path_x = "svgp_6dof_output_0.pth"
-        gp_path_y = "svgp_6dof_output_1.pth"
-        gp_path_z = "svgp_6dof_output_2.pth"
-        gp_path_wx = "svgp_6dof_output_3.pth"
-        gp_path_wy = "svgp_6dof_output_4.pth"
-        gp_path_wz = "svgp_6dof_output_5.pth"
+        gp_path = ["svgp_6dof_output_0.pth",
+                "svgp_6dof_output_1.pth",
+                "svgp_6dof_output_2.pth",
+                "svgp_6dof_output_3.pth",
+                "svgp_6dof_output_4.pth",
+                "svgp_6dof_output_5.pth"]
     else:
         knn_path = "knn_3dof.pkl"
 
-        gp_path_x = "svgp_3dof_output_0.pth"
-        gp_path_y = "svgp_3dof_output_1.pth"
-        gp_path_z = "svgp_3dof_output_2.pth"
-        gp_path_wx = "svgp_3dof_output_3.pth"
-        gp_path_wy = "svgp_3dof_output_4.pth"
-        gp_path_wz = "svgp_3dof_output_5.pth"
+        gp_path = ["svgp_3dof_output_0.pth",
+                "svgp_3dof_output_1.pth",
+                "svgp_3dof_output_2.pth",
+                "svgp_3dof_output_3.pth",
+                "svgp_3dof_output_4.pth",
+                "svgp_3dof_output_5.pth"]
     
     if model_choice == "knn":
         print(f"[startup] Loading KNN from {knn_path}")
@@ -70,47 +69,27 @@ def load_all(model_choice, dof_choice):
     else:
         print(f"[startup] Loading all 6 SVGPs")
 
-        gp_x = model_load(gp_path_x)
-        gp_y = model_load(gp_path_y)
-        gp_z = model_load(gp_path_z)
-        gp_wx = model_load(gp_path_wx)
-        gp_wy = model_load(gp_path_wy)
-        gp_wz = model_load(gp_path_wz)
+        gp_models = []
 
+        for i, path in enumerate(gp_path):
+            gp, scaler = SVGP.load(path)
+            gp_models.append(gp)
 
-def model_load(path):
-    c = torch.load(path, map_location="cpu")
-    gp_scalar = StandardScaler()
-    gp_scalar.mean_ = c["scaler_mean"]
-    gp_scalar.scale_ = c["scaler_scale"]
-    gp_model = SVGP.load(nind=c["n_inducing"], fname=path)
-    gp_model.eval()
-    gp_model.likelihood.eval()
+            if i == 0:
+                gp_scaler = scaler 
 
-    #TODO change how gp is saved in gp.py
-
-    return (gp_model)
-
-
-def model_predict(features: list[float]) -> list[float]:
+def model_predict(models, X: list[float]) -> list[float]:
     print(_model_choice + _dof_choice)
-    X = np.array(features, dtype=np.float32).reshape(1, -1)
+    X_scaled = gp_scaler.transform(X)
+
     if _model_choice == "knn":
         return knn_model.predict(X)[0].tolist()
     else:
-        predict_x = single_gp_predict(gp_x,X)
-        predict_y = single_gp_predict(gp_y,X)
-        predict_z = single_gp_predict(gp_z,X)
-        predict_wx = single_gp_predict(gp_wx,X)
-        predict_wy = single_gp_predict(gp_wy,X)
-        predict_wz = single_gp_predict(gp_wz,X)
-        return [predict_x, predict_y, predict_z, predict_wx, predict_wy, predict_wz]
-
-        #TODO check what X and mu is and make this work correctly 
-
-def single_gp_predict(gp,x):
-    mu, sigma = gp.sample(x)
-    return mu
+        predictions = []
+        for model in models:
+            mu, _ = model.sample(X_scaled)
+            predictions.append(mu[0])
+        return np.array(predictions)
 
 
 @app.post("/predict", response_class=Response)
